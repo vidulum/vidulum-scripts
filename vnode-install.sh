f7624856f4e688b0c975e5edfc722588d8bdb639
Corey M <37006206+Corey-Code@users.noreply.github.com>
2018-11-01 19:23:51 -0600

    better

--- vnode-install.sh
+++ vnode-install.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-## Vidulum Node Install Script ##
-#################################
-
+echo "    ## Vidulum Node Install Script ##   "
+echo "    #################################   "
+echo " "
 echo "----------------------------------------"
 echo "| We will help you setup your V-Node   |"
 echo "| This script will use Vidulum Release |"
 echo "----------------------------------------"
+echo " "
+echo " "
 
-echo "Please enter your Masternode Private Key GENKEY: "
-read GENKEY
+echo "Will this be a MASTERNODE? (yes or no)"
+read -i "yes" MN
 
-echo "Please enter your VPS IP: "
-read VPSIP
+if [ "$MN" == "yes" ]; then
+    echo "Please enter your Masternode Private Key GENKEY: "
+    read GENKEY
 
+    echo "Please enter your VPS IP: "
+    read VPSIP
+fi
+
+
+echo " "
 echo "-----------------------------------------------------------"
 echo "| We will not install the dependencies to run your V-Node |"
 echo "-----------------------------------------------------------"
@@ -25,42 +34,48 @@ sudo apt-get -y install \
 
 cd
 
+echo " "
 echo "------------------------------------"
 echo "| Making data and params directory |"
 echo "------------------------------------"
 mkdir .vidulum-params
 mkdir .vidulum
 
+echo " "
 echo "--------------------"
 echo "| Downloading keys |"
 echo "--------------------"
 wget -O .vidulum-params/sprout-proving.key https://gitlab.com/zcashcommunity/params/raw/master/sprout-proving.key
 wget -O .vidulum-params/sprout-verifying.key https://gitlab.com/zcashcommunity/params/raw/master/sprout-verifying.key
 
+if [ "$MN" == "yes" ]; then
+    configFile=".vidulum/vidulum.conf"
 
-configFile=".vidulum/vidulum.conf"
+    touch $configFile
+    
+    rpcuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    echo "rpcuser="$rpcuser >> $configFile
+    rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    echo "rpcpassword="$rpcpassword >> $configFile
+    echo "listen=1" >> $configFile
+    echo "server=1" >> $configFile
+    echo "txindex=1" >> $configFile
+    echo "daemon=1" >> $configFile
+    echo "masternodeaddr="$VPSIP":7676" >> $configFile
+    echo "externalip="$VPSIP":7676" >> $configFile
+    echo "masternodeprivkey="$GENKEY >> $configFile
+    echo "masternode=1" >> $configFile
+fi
 
-touch $configFile
 
-rpcuser=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo "rpcuser="$rpcuser >> $configFile
-rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo "rpcpassword="$rpcpassword >> $configFile
-echo "listen=1" >> $configFile
-echo "server=1" >> $configFile
-echo "txindex=1" >> $configFile
-echo "daemon=1" >> $configFile
-echo "masternodeaddr="$VPSIP":7676" >> $configFile
-echo "externalip="$VPSIP":7676" >> $configFile
-echo "masternodeprivkey="$GENKEY >> $configFile
-echo "masternode=1" >> $configFile
-
+echo " "
 echo "-------------------------------------"
 echo "| Downloading and unpacking Vidulum |"
 echo "-------------------------------------"
 wget https://github.com/vidulum/vidulum/releases/download/v1.0.0/vidulum-linux64.tar.gz
 tar -xvf vidulum-linux64.tar.gz
 
+echo " "
 echo "--------------------------------------------"
 echo "| Ok, it looks like everything is complete |"
 echo "|                                          |"
