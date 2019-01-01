c8ef6453167e5b910f441bd65df1f50ac332ab39
Corey M <37006206+Corey-Code@users.noreply.github.com>
2019-01-01 14:58:29 -0700

    testing it

--- upgrade_node.sh
+++ upgrade_node.sh
@@ -1,1 +1,29 @@
 #Upgrade Node to current release
+
+echo " "
+echo " "
+echo "Upgrading Vidulum daemon and client"
+echo "V I D U L U M   B L O C K C H A I N"
+
+
+./vidulum-cli stop
+
+echo " "
+echo " "
+echo "I take a nap"
+echo "I take a nap right here for 5 seconds"
+sleep 5
+
+rm vidulumd
+rm vidulum-cli
+wget -q --show-progress https://github.com/vidulum/vidulum/releases/download/v1.0.1/vidulum-linux64.tar.gz
+tar -xzf vidulum-linux64.tar.gz
+
+rm vidulum-linux64.tar.gz
+mv vidulum-release/vidulumd .
+mv vidulum-release/vidulum-cli .
+
+./vidulumd -daemon=1
+echo " "
+echo " "
+echo "See what was easy!  ./vidulum-cli getinfo "
