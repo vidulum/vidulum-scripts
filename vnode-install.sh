180b0558f875bf74cfca7763c0a6d29c11349f72
Corey M <37006206+Corey-Code@users.noreply.github.com>
2019-01-03 16:02:38 -0700

    Adjust for new release

--- vnode-install.sh
+++ vnode-install.sh
@@ -169,6 +169,8 @@ else
 wget https://github.com/vidulum/vidulum/releases/download/v1.0.1/vidulum-linux64.tar.gz
 tar -xvf vidulum-linux64.tar.gz
 rm vidulum-linux64.tar.gz
+cp vidulum-release/vidulumd.
+cp vidulum-release/vidulum-cli.
 fi
 
 echo " "
