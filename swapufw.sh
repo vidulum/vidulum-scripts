21afda7d4e81209d9c7550bfdb888950246badc8
J <jeremyd88@gmail.com>
2019-07-05 07:10:03 -0400

    swapfile/ufw script

--- swapufw.sh
+++ swapufw.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-#
-#
+# wget https://raw.githubusercontent.com/vidulum/vidulum-scripts/master/swapufw.sh
+# chmod u+x swapufw.sh
+# ./swapufw.sh
 #
 echo " "
 echo "-------------------------------------------------"
@@ -11,20 +12,32 @@ echo "|  memory out of you nodes.                     |"
 echo "|                                               |"
 echo "|  Also, we will setup the UFW firewall on      |"
 echo "|  Ubuntu.                                      |"
+echo "|                                               |"
 echo "|  Please make sure your VPS provider allows    |"  
 echo "|  swapfiles. Vultr does, and we recommend them |"
 echo "------------------------------------------------|"
 echo " "
 
 echo "Would you like to install a swapfile on your VPS? (yes/no)"
-read -i SW       
+read -i "yes" SW       
 
 if [[ $SW =~ [yY](es)* ]]; then
 
 echo "Would you like it to be 2GB or 4GB? (2/4)"
-read -i SIZE
+read size
 
-dd if=/dev/zero of=/swapfile count=${SIZE} bs=1M
+if [ $size == "2" ]; then
+SWAP=2048
+elif [ $size == "4" ]; then
+SWAP=4096
+else
+exit 1
+fi
+
+echo "Creating Swapfile"
+sleep 3
+
+dd if=/dev/zero of=/swapfile count="$SWAP" bs=1M
 
 chmod 600 /swapfile
 
@@ -36,50 +49,36 @@ echo "/swapfile  none  sw  0  0" >> /etc/fstab
 
 echo "Swapfile installed!"
 
-sleep 10
+sleep 3
 
-else 
-
+else
+echo " "
 echo "We will not be installing a swapfile"
 echo " "
 fi
 
 echo "Would you like to configure the firewall? (yes/no)"
-read -i FW 
+read -i "yes" FW 
 
 if [[ $FW =~ [yY](es)* ]]; then
+sleep 3
+
+echo "Installing Firewall"
 
 apt-get -y install ufw 
 ufw allow ssh/tcp
 ufw limit ssh/tcp
 ufw allow 7676
 ufw logging on
-ufw enable
-ufw status
+ufw reload
 
 echo " "
 echo "Firewall is enabled for a Vidulum Node!"
 
-sleep 10
+sleep 2
 
 else 
 
 echo "Firewall will not be configured"
 
 fi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
